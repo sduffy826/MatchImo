4cbeecfdbda9c7491a1a71aecdd1211004cc0c10
Sean Duffy <remi_96@hotmail.com>
2014-10-07 18:22:59 -0400

    Just trying to publish from desktop and pull to laptop

--- MatchNew/Model/CardMatchingGame.h
+++ MatchNew/Model/CardMatchingGame.h
@@ -12,7 +12,7 @@
 
 @interface CardMatchingGame : NSObject
 
-// designated initializer
+// designated initializer, use it :)
 - (instancetype)initWithCardCount:(NSUInteger)count
                         usingDeck:(Deck *)deck;
 
